594b87108773d858ef777d1739643abafdeeff1e
MarcoL12 <56522294+MarcoL12@users.noreply.github.com>
2019-12-08 16:25:56 -0600

    Formulario y Foro update

---
+++ public/js/formulario.js
@@ -0,0 +1,11 @@
+function registrar() {
+  user = document.getElementById('user').value;
+  correo = document.getElementById('correo').value;
+  psswd = document.getElementById('psswd').value;
+  nombre = document.getElementById('nombre').value;
+  macho = document.getElementById('macho').value;
+  hembra = document.getElementById('hembra').value;
+  edad = document.getElementById('edad').value;
+  raza = document.getElementById('raza').value;
+  comentario = document.getElementById('comentario').value;
+}
---
+++ public/js/foro.js
@@ -0,0 +1,5 @@
+function sendComment(){
+  comment = document.getElementById('comments').value;
+  containerForo = document.getElementById('foro');
+  containerForo.innerHTML += "<div id='foro'>" + "<p id='comm'>" + "User: &nbsp " + comment + "</p>" + "</div>";
+}
